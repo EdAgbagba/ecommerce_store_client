acc1f54364ef314af99a059625efe393e02114f1
AmakaOrji <chiamakaorji17@gmail.com>
2022-12-07 08:33:02 +0000

    new layout

--- createAccount.js
+++ createAccount.js
@@ -1,18 +1,41 @@
-function createAccount(){
-    let account={
-        firstName : document.getElementById('firstname').value,
 
-        lastname : document.getElementById('lastname').value,
-        businessname : document.getElementById('Businessname').value,
+function LogIn(){
+  let x = document.getElementById('email').value;
+  let y = document.getElementById('password').value;
 
+  if (x ===""){
+    alert("Email is required");
+    return;
+ 
+  }else if (y ===""){
+    alert("Password is required");
+    return;
 
-        phonenumber : document.getElementById('phone').value,
-
-        emailaddress : document.getElementById('emailaddress').value,
-
-        password: document.getElementById('password').value,
-
-        confirmPassword: document.getElementById('cmfPswd'),
-    }  
-    console.log(account);
   }
+  
+}
+
+function createAccount(){
+  let Password = document.getElementById('password').value;
+  let Confirm = document.getElementById('confirmPassword')
+}
+
+
+// function createAccount(){
+//     let account={
+//         firstName : document.getElementById('firstname').value,
+
+//         lastname : document.getElementById('lastname').value,
+//         businessname : document.getElementById('Businessname').value,
+
+
+//         phonenumber : document.getElementById('phone').value,
+
+//         emailaddress : document.getElementById('emailaddress').value,
+
+//         password: document.getElementById('password').value,
+
+//         confirmPassword: document.getElementById('cmfPswd'),
+//     }  
+//     console.log(account);
+//   }
