8ad87c98cd7404d2cfdfb4495f916404a7ced9f7
EdAgbagba <EdAgbagba@gmail.com>
2022-12-08 15:32:15 +0000

    personax page changes

--- utils.js
+++ utils.js
@@ -1,14 +1,20 @@
 let selectedProduct = function(name) {
   let mychoice;
   let dim;
+  let colorLite_Left = "#11c3022b"
+  let colorStandard_left = "#001e8a2b"
+  let circleRight = document.getElementById("right-circle")
     
   if (name === "lite") {
     dim = document.getElementById("lite");
     if (dim.classList.contains("dimmer")) {
       dim.classList.remove("dimmer");
       document.getElementById("standard").classList.add("dimmer");
+      
     }
      else document.getElementById("standard").classList.add("dimmer");
+     document.getElementById("circle-left").style.background = colorLite_Left;
+    circleRight.src = "images/lite.svg"
     mychoice = dim.innerHTML.slice(2);
    
   } 
@@ -18,6 +24,8 @@ let selectedProduct = function(name) {
       dim.classList.remove("dimmer");
       document.getElementById("lite").classList.add("dimmer");
     } else document.getElementById("lite").classList.add("dimmer");
+    document.getElementById("circle-left").style.background = colorStandard_left;
+    circleRight.src = "images/standard.svg"
     mychoice = dim.innerHTML.slice(2);
     
   }
